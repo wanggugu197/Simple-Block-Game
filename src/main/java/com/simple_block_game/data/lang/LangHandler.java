b5e9fc0b70a988153cb38c4fd90dab7c7007889f
wanggugu <87158048+wanggugu197@users.noreply.github.com>
2026-06-09 18:27:29 +0800

    fix

--- src/main/java/com/simple_block_game/data/lang/LangHandler.java
+++ src/main/java/com/simple_block_game/data/lang/LangHandler.java
@@ -5,7 +5,7 @@ import static com.simple_block_game.SimpleBlockGame.REGISTRYLIB;
 public class LangHandler {
 
     public static void addLang(String key, String cn, String en) {
-        REGISTRYLIB.lang("en_us", key, en);
+        REGISTRYLIB.lang(key, en);
         REGISTRYLIB.lang("zh_cn", key, cn);
     }
 
